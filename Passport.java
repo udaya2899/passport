253ea25d6d961cd3255488201b59f2e94977ddfb
Udaya Prakash <udayaprakash2899@gmail.com>
2018-07-12 18:29:22 +0530

    Nothing changed except comments

--- Passport.java
+++ Passport.java
@@ -13,10 +13,7 @@ import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
 
-/**
- *
- * @author user
- */
+
 public class Passport extends Application {
     
     @Override
@@ -25,13 +22,11 @@ public class Passport extends Application {
         stage.initStyle(StageStyle.UNDECORATED);
         Scene scene = new Scene(root);
         
-        stage.setScene(scene);
+  	stage.setScene(scene);
         stage.show();
     }
 
-    /**
-     * @param args the command line arguments
-     */
+ 
     public static void main(String[] args) {
         launch(args);
     }
